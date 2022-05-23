4b1df1eb308a55178485e359d2e881bea8b07516
username <wkadlf2310@gmail.com>
2022-05-23 23:59:39 +0900

    feat: Implement bottom navigation menu
    네비게이션 바 구현 후 프레그먼트와 액티비티와 연동 완료

--- app/src/main/java/smu/app/nunsong_market/MainActivity.kt
+++ app/src/main/java/smu/app/nunsong_market/MainActivity.kt
@@ -1,11 +1,66 @@
 package smu.app.nunsong_market
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import smu.app.nunsong_market.databinding.ActivityMainBinding
+import smu.app.nunsong_market.fragment.MessageFragment
+import smu.app.nunsong_market.fragment.HomeFragment
+import smu.app.nunsong_market.fragment.MyPageFragment
 
 class MainActivity : AppCompatActivity() {
+
+    // 멤버 변수 선언
+    private  lateinit var binding: ActivityMainBinding
+
+    private val  bottomNavigationView: BottomNavigationView by lazy {
+        binding.bottomNavigation
+    }
+    private val homeFragment: HomeFragment by lazy {
+        HomeFragment.newInstance()
+    }
+    private val chattingFragment: MessageFragment by lazy {
+        MessageFragment.newInstance()
+    }
+    private val myPageFragment: MyPageFragment by lazy {
+        MyPageFragment.newInstance()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // default fragment
+        supportFragmentManager.beginTransaction().add(R.id.container,homeFragment).commit()
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item->
+            when(item.itemId){
+                R.id.page_home->{
+                    supportFragmentManager.beginTransaction().replace(R.id.container,homeFragment).commit()
+
+                    true
+                }
+                R.id.page_post->{
+                    val intent = Intent(this,PublishActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.page_chatting->{
+                    supportFragmentManager.beginTransaction().replace(R.id.container,chattingFragment).commit()
+
+                    true
+                }
+                R.id.page_mypage->{
+                    supportFragmentManager.beginTransaction().replace(R.id.container,myPageFragment).commit()
+                    true
+                }
+                else -> false
+            }
+        }
+
+
     }
 }
---
+++ app/src/main/java/smu/app/nunsong_market/PublishActivity.kt
@@ -0,0 +1,11 @@
+package smu.app.nunsong_market
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PublishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_publish)
+    }
+}
